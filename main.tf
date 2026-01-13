8bdc2390a56e9326ac2bc97a21e4fbf354baeb71
Rohit <rohitsahu269@gmail.com>
2026-01-13 22:10:46 +0530

    Clean terraform repo

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rohit-tfstate-rg"
+    storage_account_name = "rohittfstatestorage123"
+    container_name       = "rohit-terraform-state"
+    key                  = "rohit-jenkins.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rohit-jenkins-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "rohit-jenkins-vnet"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
